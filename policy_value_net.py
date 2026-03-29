f8c1576d382aefebc3a3693e7553d104db73dba5
Ren <ren970828438@gmail.com>
2026-03-28 17:08:03 -0700

    added BN

--- policy_value_net.py
+++ policy_value_net.py
@@ -18,33 +18,38 @@ class Net(nn.Module):
         self.board_width = board_width
         self.board_height = board_height
 
-        # common layers
+        # common layers (Conv-BN-ReLU)
         self.conv1 = nn.Conv2d(4, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
 
         # action policy layers (policy network)
         self.act_conv1 = nn.Conv2d(128, 4, kernel_size=1)
+        self.bn_act = nn.BatchNorm2d(4)
         self.act_fc1 = nn.Linear(4*board_width*board_height, board_width*board_height)
 
         # state value layers (value network)
         self.val_conv1 = nn.Conv2d(128, 2, kernel_size=1)
+        self.bn_val = nn.BatchNorm2d(2)
         self.val_fc1 = nn.Linear(2*board_width*board_height, 64)
         self.val_fc2 = nn.Linear(64, 1)
 
     def forward(self, state_input):
         # common layers
-        x = F.relu(self.conv1(state_input))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
+        x = F.relu(self.bn1(self.conv1(state_input)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
 
         # action policy layers
-        x_act = F.relu(self.act_conv1(x))
+        x_act = F.relu(self.bn_act(self.act_conv1(x)))
         x_act = x_act.view(-1, 4*self.board_width*self.board_height)
         x_act = F.log_softmax(self.act_fc1(x_act), dim=1)
 
         # state value layers
-        x_val = F.relu(self.val_conv1(x))
+        x_val = F.relu(self.bn_val(self.val_conv1(x)))
         x_val = x_val.view(-1, 2*self.board_width*self.board_height)
         x_val = F.relu(self.val_fc1(x_val))
         x_val = torch.tanh(self.val_fc2(x_val))
